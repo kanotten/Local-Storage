cf19abde6573c2efa18d5d1c5f0c51b82374abab
Sheikh <kenneth.sheikh@hotmail.com>
2024-01-12 12:58:16 +0100

    added file for session storage

--- main.js
+++ main.js
@@ -6,3 +6,14 @@ window.localStorage.setItem("name", "Hesham");
 //save objects
 //save arrays
 
+let car = {
+    make: "bmw",
+    model: 1992,
+    price: 2000
+}
+
+//must be turned to a string to convert/import/export
+
+window.localStorage.setItem("myCar", JSON.stringify(car));
+
+window.sessionStorage.setItem("fav", "bmw");
--- script2.js
+++ script2.js
@@ -1,4 +1,10 @@
 const myName = window.localStorage.getItem("name");
 console.log(myName);
 
+const myCar = window.localStorage.getItem("myCar");
+console.log(myCar);
 
+// parse is to convert back to javascript from string fromat
+
+const myCarParsed = JSON.parse(myCar);
+console.log(myCarParsed);
